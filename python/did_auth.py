1d18b9982c5d5ae3884f0e314c9d8237b2aad8a7
Kyle Den Hartog <kdenhar@gmail.com>
2018-10-12 03:27:16 -0600

    added onboarding fn
    Signed-off-by: Kyle Den Hartog <kdenhar@gmail.com>

--- python/did_auth.py
+++ python/did_auth.py
@@ -22,6 +22,25 @@ async def run():
     # Set protocol version 2 to work with Indy Node 1.4
     await pool.set_protocol_version(PROTOCOL_VERSION)
 
+    logger.info("==========================================")
+    logger.info("=== Setting up Sovrin Steward's Wallet ===")
+    logger.info("------------------------------------------")
+    
+    logger.info("\"Sovrin Steward\" -> Create wallet")
+    steward_wallet_config = json.dumps({"id": "sovrin_steward_wallet"})
+    steward_wallet_credentials = json.dumps({"key": "steward_wallet_key"})
+    try:
+        await wallet.create_wallet(steward_wallet_config, steward_wallet_credentials)
+    except IndyError as ex:
+        if ex.error_code == ErrorCode.WalletAlreadyExistsError:
+            pass
+
+    steward_wallet = await wallet.open_wallet(steward_wallet_config, steward_wallet_credentials)
+
+    logger.info("\"Sovrin Steward\" -> Create and store in Wallet DID from seed")
+    steward_did_info = {'seed': '000000000000000000000000Steward1'}
+    (steward_did, steward_key) = await did.create_and_store_my_did(steward_wallet, json.dumps(steward_did_info))
+
     try:
         await pool.create_pool_ledger_config(pool_name, pool_config)
     except IndyError as ex:
@@ -42,22 +61,79 @@ async def run():
         if ex.error_code == ErrorCode.WalletAlreadyExistsError:
             pass
 
-    logger.info("=================================")
-    logger.info("=== Setting up Bob's Wallet ===")
-    logger.info("---------------------------------")
+    logger.info("==============================================")
+    logger.info("=== Onboarding A+ Accountants to ledger =====")
+    logger.info("----------------------------------------------")
 
     logger.info("\"Bob\" -> Create wallet")
-    bob_wallet_config = json.dumps({"id": "bob_wallet"})
-    bob_wallet_credentials = json.dumps({"key": "bob_wallet_key"})
-    try:
-        await wallet.create_wallet(bob_wallet_config, bob_wallet_credentials)
-    except IndyError as ex:
-        if ex.error_code == ErrorCode.WalletAlreadyExistsError:
-            pass
+    employer_wallet_config = json.dumps({"id": "A+_Accountant_wallet"})
+    employer_wallet_credentials = json.dumps({"key": "a+_accountant_wallet_key"})
+    
+    employer_wallet, steward_employer_key, faber_employer_did, employer_steward_key, _ = \
+        await onboarding(pool_handle, "Sovrin Steward", steward_wallet, steward_did, "A+ Accountants", None, faber_wallet_config, faber_wallet_credentials)
 
-    
-    
-    
+    logger.info("==================================================")
+    logger.info("=== Setting up ACME Accounting WebApp's Wallet ===")
+    logger.info("--------------------------------------------------")
+
+    logger.info("\"Bob\" -> Create wallet")
+    acme_wallet_config = json.dumps({"id": "acme_wallet"})
+    acme_wallet_credentials = json.dumps({"key": "acme_wallet_key"})
+
+
+async def onboarding(pool_handle, _from, from_wallet, from_did, to, to_wallet: Optional[str], to_wallet_config: str,
+                     to_wallet_credentials: str):
+    logger.info("\"{}\" -> Create and store in Wallet \"{} {}\" DID".format(_from, _from, to))
+    (from_to_did, from_to_key) = await did.create_and_store_my_did(from_wallet, "{}")
+
+    logger.info("\"{}\" -> Send Nym to Ledger for \"{} {}\" DID".format(_from, _from, to))
+    await send_nym(pool_handle, from_wallet, from_did, from_to_did, from_to_key, None)
+
+    logger.info("\"{}\" -> Send connection request to {} with \"{} {}\" DID and nonce".format(_from, to, _from, to))
+    connection_request = {
+        'did': from_to_did,
+        'nonce': 123456789
+    }
+
+    if not to_wallet:
+        logger.info("\"{}\" -> Create wallet".format(to))
+        try:
+            await wallet.create_wallet(to_wallet_config, to_wallet_credentials)
+        except IndyError as ex:
+            if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
+                pass
+        to_wallet = await wallet.open_wallet(to_wallet_config, to_wallet_credentials)
+
+    logger.info("\"{}\" -> Create and store in Wallet \"{} {}\" DID".format(to, to, _from))
+    (to_from_did, to_from_key) = await did.create_and_store_my_did(to_wallet, "{}")
+
+    logger.info("\"{}\" -> Get key for did from \"{}\" connection request".format(to, _from))
+    from_to_verkey = await did.key_for_did(pool_handle, to_wallet, connection_request['did'])
+
+    logger.info("\"{}\" -> Anoncrypt connection response for \"{}\" with \"{} {}\" DID, verkey and nonce"
+                .format(to, _from, to, _from))
+    connection_response = json.dumps({
+        'did': to_from_did,
+        'verkey': to_from_key,
+        'nonce': connection_request['nonce']
+    })
+    anoncrypted_connection_response = await crypto.anon_crypt(from_to_verkey, connection_response.encode('utf-8'))
+
+    logger.info("\"{}\" -> Send anoncrypted connection response to \"{}\"".format(to, _from))
+
+    logger.info("\"{}\" -> Anondecrypt connection response from \"{}\"".format(_from, to))
+    decrypted_connection_response = \
+        json.loads((await crypto.anon_decrypt(from_wallet, from_to_key,
+                                              anoncrypted_connection_response)).decode("utf-8"))
+
+    logger.info("\"{}\" -> Authenticates \"{}\" by comparision of Nonce".format(_from, to))
+    assert connection_request['nonce'] == decrypted_connection_response['nonce']
+
+    logger.info("\"{}\" -> Send Nym to Ledger for \"{} {}\" DID".format(_from, to, _from))
+    await send_nym(pool_handle, from_wallet, from_did, to_from_did, to_from_key, None)
+
+    return to_wallet, from_to_key, to_from_did, to_from_key, decrypted_connection_response
+
 if __name__ == '__main__':
     run_coroutine(run)
     time.sleep(1)  # FIXME waiting for libindy thread complete
