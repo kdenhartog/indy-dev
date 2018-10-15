24abeea6f6c5deb9e08dbfead3b0f30f1a301227
Kyle Den Hartog <kdenhar@gmail.com>
2018-10-15 02:20:13 -0600

    fix build script to match directory structure
    Signed-off-by: Kyle Den Hartog <kdenhar@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f ./scripts/indy-pool.dockerfile -t indy_pool .
+docker build -f indy-pool.dockerfile -t indy_pool .
 echo "Finished building indy_pool image"
-docker build -f ./scripts/indy-dev.dockerfile -t indy_dev .
+docker build -f indy-dev.dockerfile -t indy_dev .
 echo "Finished building indy_dev image"
