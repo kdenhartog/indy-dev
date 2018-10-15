4d99ca13ba91b70887a8829a050f5b4ad50633a4
bryant <blhagadorn@gmail.com>
2018-10-15 01:34:53 -0500

    Changed scripts so they work for me and do proper cleanup
    Signed-off-by: bryant <blhagadorn@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 #Stop and remove all running Indy containers
-docker rmi $(docker images -a | grep indy | awk '{print $1}')
+docker run -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_pool
+echo "Indy_pool running"
+echo "You are now in the indy_dev environment command line"
+docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
