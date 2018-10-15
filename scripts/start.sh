d953777c1946ff802c872c3ed3a4218dc84f5f71
Kyle Den Hartog <kdenhar@gmail.com>
2018-10-15 16:37:48 -0600

    fixed scripts
    Signed-off-by: Kyle Den Hartog <kdenhar@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 #Stop and remove all running Indy containers
-docker rmi $(docker images -a | grep indy | awk '{print $1}')
+if [ "$(docker ps -a -f status=running | grep indy_pool)" ]
+    then
+        docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
+    else
+        docker run -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_pool
+        docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
+fi
