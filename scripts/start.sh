faccb86e00dba9157e145dbf8b96b139b375e1fc
Kyle Den Hartog <kdenhartog@users.noreply.github.com>
2018-10-16 10:22:56 -0600

    Delete start.sh
    using another start.sh script I built, that allows starting only indy_dev if indy_pool is already running.

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Stop and remove all running Indy containers
-docker run -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_pool
-echo "Indy_pool running"
-echo "You are now in the indy_dev environment command line"
-docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
