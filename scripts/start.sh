8e08dd4f538d59d41e6ca5dcdf5b577c687c48fd
Kyle Den Hartog <kdenhar@gmail.com>
2018-10-17 11:12:17 -0600

    fix start script
    Signed-off-by: Kyle Den Hartog <kdenhar@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -f indy-pool.dockerfile -t indy_pool .
-docker build -f indy-dev.dockerfile -t indy_dev .
+docker run -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_pool
+docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
